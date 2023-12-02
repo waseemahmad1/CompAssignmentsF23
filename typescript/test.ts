77dbce4248e6a5b47397c5774513bd4ac9225093
Waseem <133596689+waseemahmad1@users.noreply.github.com>
2023-12-01 22:42:15 -0500

    Update test.ts

--- typescript/test.ts
+++ typescript/test.ts
@@ -1,8 +1,19 @@
 function square(n) {
     return 0;
+function square(n: unknown): number {
+    if (typeof n === 'number')
+    {
+        return n * n;
+    }
+    return -1;
 }
 
-
-function numberString(i) {
-    return ""; 
-}
+function numberString(i: number): string | number {
+    let result: string = "";    
+    if (i % 1 == 0 && i > 0) {
+        for (let c = 1; c <= i; c++) {
+        result += c.toString();
+        }
+    return result;
+    }
+    return -1;
